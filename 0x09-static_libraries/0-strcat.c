e457258af42038e5fcb5be18758deffce8fe8452
Ray90kotey <raykotey5@gmail.com>
2023-04-11 19:10:12 -0700

    fix

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,10 +1,15 @@
 #include "main.h"
- /**
+/**
  * _strcat - concatenates two strings
  * @dest: destination string
  * @src: source string
  *
- * Return: void
+ * Description: The _strcat function appends the src string to the dest string,
+ * overwriting the terminating null byte (\0) at the end of dest, and then adds
+ * a terminating null byte. The strings may not overlap, and the dest string
+ * must have enough space for the result.
+ *
+ * Return: a pointer to the resulting string dest
  */
 char *_strcat(char *dest, char *src)
 {
